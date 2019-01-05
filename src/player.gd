9488755a1764c1a875aa9b4f5caf2499d42eb02d
Heath Milligan <heath@heathmilligan.net>
2019-01-05 13:38:02 -0500

    New physics

--- src/player.gd
+++ src/player.gd
@@ -3,16 +3,16 @@ extends KinematicBody2D
 # This demo shows how to build a kinematic controller.
 
 # Member variables
-const GRAVITY = 500.0 # pixels/second/second
+const GRAVITY = 1500.0 # pixels/second/second
 
 # Angle in degrees towards either side that the player can consider "floor"
 const FLOOR_ANGLE_TOLERANCE = 40
-const WALK_FORCE = 600
-const WALK_MIN_SPEED = 10
-const WALK_MAX_SPEED = 200
-const STOP_FORCE = 1300
-const JUMP_SPEED = 200
-const JUMP_MAX_AIRBORNE_TIME = 0.2
+const WALK_FORCE = 800
+const WALK_MIN_SPEED = 30
+const WALK_MAX_SPEED = 400
+const STOP_FORCE = 2000
+const JUMP_SPEED = 1000
+const JUMP_MAX_AIRBORNE_TIME = 0.4
 
 const SLIDE_STOP_VELOCITY = 1.0 # one pixel/second
 const SLIDE_STOP_MIN_TRAVEL = 1.0 # one pixel
