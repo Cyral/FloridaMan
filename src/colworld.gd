a546a9168a0d89b47ce37e462160a47eda34cf24
Anthony H <the7bunsoflife@gmail.com>
2019-01-05 13:23:15 -0500

    Add files via upload
    You now need to collect pubsubs in order to pass the level

--- src/colworld.gd
+++ src/colworld.gd
@@ -1,7 +1,16 @@
 extends Node2D
 export(String, FILE, "*.tscn") var world_scene
 
+var pubsub_counter = 0
+
 func _on_princess_body_enter(body):
 	# The name of this editor-generated callback is unfortunate
-	if body.get_name() == "player":
+	if body.get_name() == "player" and pubsub_counter >= 1:
 		get_tree().change_scene(world_scene)
+
+
+# When the player touches the pubsub it deletes it
+func _on_Pubsub_body_entered(body):
+	pubsub_counter += 1
+	$Pubsub.queue_free()
+	
